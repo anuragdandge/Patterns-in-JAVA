8a42fc03b011761be9c06573de00ecd6f1c5e062
Anurag Dandge <41123540+anuragdandge@users.noreply.github.com>
2022-08-19 21:16:38 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,146 @@
+
+public class PdfPattern {
+    public static void main(String[] args) {
+        // System.out.println(" Q. 2 ");
+        // for (int i = 0; i < 10; i += 2) {
+        // for (int j = 0; j < i; j += 2) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 5; j > (i / 2); j--) {
+        // System.out.print(" * ");
+        // }
+        // System.out.println("");
+        // }
+
+        // System.out.println("Q. 3 ");
+        // for (int i = 0; i < 5; i++) {
+        // for (int j = 5; j > i; j--) {
+        // System.out.print("   ");
+        // }
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print(" * ");
+        // }
+        // System.out.println(" ");
+        // }
+
+        
+        // System.out.println("Q. 4");
+        // for (int i = 0; i < 5; i++) {
+        // for (int j = 5; j > i; j--) {
+        // System.out.print("  ");
+        // }
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print(" *");
+        // }
+        // System.out.println("");
+        // }
+        // for (int i = 0; i < 5; i++) {
+        // for (int j = -1; j <= i; j++) {
+        // System.out.print("  ");
+        // }
+        // for (int j = 4; j > i; j--) {
+        // System.out.print(" *");
+        // }
+        // System.out.println("");
+        // }
+
+
+        // System.out.println("Q 5");
+        // for (int i = 0; i < 3; i++) {
+        // for (int j = 3; j > i; j--) {
+        // System.out.print(" * ");
+        // }
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print("      ");
+        // }
+        // for (int j = 3; j > i; j--) {
+        // System.out.print(" * ");
+        // }
+        // System.out.println("");
+        // }
+        // System.out.println("");
+        // for (int i = 0; i < 3; i++) {
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print(" * ");
+        // }
+        // for (int j = 3; j > i; j--) {
+        // System.out.print("      ");
+        // }
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print(" * ");
+        // }
+        // System.out.println("");
+        // }
+
+
+        // System.out.println(" Q 6 ");
+        // int n = 0;
+        // for (int i = 1; i < 6; i++) {
+        // for (int space = 6; space > i; space--) {
+        // System.out.print("  ");
+        // }
+        // for (int j = 0; j < i; j++) {
+        // n = i + j;
+        // }
+        // for (int k = 1; k <= n; k++) {
+        // System.out.print(" " + k + "");
+        // }
+        // System.out.println();
+        // }
+
+        // System.out.println("Q 7");
+        // int num = 6;
+        // int n = 0;
+        // for (int i = 1; i <= num; i++) {
+        //     for (int space = num; space > i; space--) {
+        //         System.out.print("   ");
+        //     }
+        //     for (int j = 0; j < i; j++) {
+        //         n = i + j;
+        //     }
+        //     for (int k = 1; k <= n; k++) {
+        //         if (k == 2) {
+        //             System.out.print(" " + i + " ");
+        //         }
+        //         if (k != 1 && k != n) {
+        //             System.out.print(" 0 ");
+        //         }
+        //         if (k == n) {
+        //             System.out.print(" " + i);
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println(" Q 8");
+        // int n = 9;
+        // for (int i = 0; i <= n; i++) {
+        // for (int space = n; space > i; space--) {
+        // System.out.print(" ");
+        // }
+        // for (int j = ((n - i) + 1); j <= n; j++) {
+        // System.out.print(" " + j);
+        // }
+        // System.out.print(" 0");
+        // for (int j = n; j > (n - i); j--) {
+        // System.out.print(" " + j);
+        // }
+        // System.out.println();
+        // }
+
+        // System.out.println(" Q 9 ");
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // for (int space = n; space > i; space--) {
+        // System.out.print(" ");
+        // }
+        // if( i >= 2 && i == n){
+        // for(int j = 1 ; j <= i ;j++){
+        // System.out.print("*");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+    }
+}
